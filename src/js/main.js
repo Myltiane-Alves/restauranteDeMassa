869dc018240e609a72a5f77bd5f37565af30493f
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-19 16:44:48 -0300

    Projeto finalizado

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  navbar.classList.remove("show");
+});
+
+/* Testimonial Slider */
+const swiper = new Swiper(".myslider", {
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  autoHeight: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".custom-btn ion-icon, .custom-btn",
+  },
+});
